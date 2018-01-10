ac953689770febc11f43419bd395ad24940afd61
IgorFigueira <igorffigueira@gmail.com>
2018-01-10 15:52:50 +0000

    Route_Controller_View Login

--- ManageMuseum/ManageMuseum/App_Start/RouteConfig.cs
+++ ManageMuseum/ManageMuseum/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace ManageMuseum
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ ManageMuseum/ManageMuseum/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManageMuseum.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Login()
+        {
+            return View("~/Views/Login/Login.cshtml");
+        }
+    }
+}
