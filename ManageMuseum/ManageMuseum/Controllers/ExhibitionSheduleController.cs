300f215b8a3e7036d3e4664397f1dc2337f76648
José Tiago Mendonça Gouveia <jose.t.m.gouveia@gmail.com>
2018-01-17 02:48:38 +0000

    Getting data from Database to show on EventRequests

--- ManageMuseum/ManageMuseum/Controllers/ExhibitionSheduleController.cs
+++ ManageMuseum/ManageMuseum/Controllers/ExhibitionSheduleController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ManageMuseum.Models;
 
 namespace ManageMuseum.Controllers
 {
@@ -11,6 +13,25 @@ namespace ManageMuseum.Controllers
         // GET: ExhibitionShedule
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult ShowRequestsList()
+        {
+            var db = new OurContectDb();
+
+            var query = db.Events.Include(d => d.EventState).Include(d => d.EventType).Where(d => d.EventState.Id == 1 && d.EventType.Id == 1);
+            var mira = query.ToList();
+
+            foreach (var item in mira)
+            {
+
+                var see = item.EventState;
+               Console.WriteLine(see);
+            }
+            
+            
+            
             return View();
         }
     }
