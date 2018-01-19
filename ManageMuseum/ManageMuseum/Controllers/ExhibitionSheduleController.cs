1a2d4891a104231f368b87126233021101e9988f
IgorFigueira <igorffigueira@gmail.com>
2018-01-19 16:32:52 +0000

    EventDetails

--- ManageMuseum/ManageMuseum/Controllers/ExhibitionSheduleController.cs
+++ ManageMuseum/ManageMuseum/Controllers/ExhibitionSheduleController.cs
@@ -25,11 +25,40 @@ namespace ManageMuseum.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult EventRequestDetails()
+        public ActionResult EventRequestApprove()
         {
+            return Content("Aprovar pedido");
+        }
+       
+        public ActionResult EventRequestDetails(string EventId)
+        {
+            var EventIdSelected = Int32.Parse(EventId);
+            ViewData["EventIdSelected"] = EventIdSelected;
             return View();
         }
 
+        //public ActionResult listaQUalquercoisa()
+        //{
+            
+        //}
+
     }
 }
+
+
+//
+//var eventDetails = Int32.Parse(collection["Events.Id"]);
+//if (eventDetails != null)
+//{
+//    Event evento = new Event();
+
+//    evento.Id = eventDetails.Id;
+//    evento.Name = eventDetails.Name;
+//    evento.Description = eventDetails.Description;
+//    evento.EventType = eventDetails.EventType;
+//    evento.StartDate = eventDetails.StartDate;
+//    evento.EnDate = eventDetails.EnDate;
+//    ViewData.Model = evento;
+
+//}
+//return View(a);
