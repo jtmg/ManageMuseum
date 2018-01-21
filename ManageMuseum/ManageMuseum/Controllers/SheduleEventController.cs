a497d62a02622420f2701da9ec0c644148808123
José Tiago Mendonça Gouveia <jose.t.m.gouveia@gmail.com>
2018-01-21 01:42:51 +0000

    -.-

--- ManageMuseum/ManageMuseum/Controllers/SheduleEventController.cs
+++ ManageMuseum/ManageMuseum/Controllers/SheduleEventController.cs
@@ -33,7 +33,7 @@ namespace ManageMuseum.Controllers
         {
             var eventType = events.EventType;
             var getEventTypeRow = db.EventTypes.FirstOrDefault(s => s.Name == eventType);
-            var eventState = db.EventStates.Single(s => s.Id == 1);
+            var eventState = db.EventStates.Where(s => s.Id == 1).Single();
             var userId = Int32.Parse(Request.Cookies["UserId"].Value);
             var userAccount = db.UserAccounts.Include(d=>d.Role).FirstOrDefault(s => s.Id == userId);
             var finalEvent = new Event() {Name = events.Name, StartDate = events.StartDate, EnDate = events.EnDate,Description = events.Description,EventType = getEventTypeRow, EventState = eventState,UserAccount = userAccount};
