c2eff40c943ff25f814ef4d6cc2a9342e2eeacb0
José Tiago Mendonça Gouveia <jose.t.m.gouveia@gmail.com>
2018-01-21 00:32:54 +0000

    listar pedidos aceites

--- ManageMuseum/ManageMuseum/Controllers/SheduleEventController.cs
+++ ManageMuseum/ManageMuseum/Controllers/SheduleEventController.cs
@@ -27,13 +27,29 @@ namespace ManageMuseum.Controllers
             return View();
         }
 
-       
+        public ActionResult ApprovedExhibition()
+        {
+            var userId = Int32.Parse(Request.Cookies["UserId"].Value);
+            var eventType = db.EventTypes.Single(s => s.Name == "exposicao");
+            var eventState = db.EventStates.Single(s=>s.Name == "aceites");
+            var eventsToShow = db.Events.Include(d => d.UserAccount).Include(d=>d.EventType).Include(d=>d.EventState).Where(d=>d.UserAccount.Id == userId && d.EventType.Id == eventType.Id &&d.EventState.Id == eventState.Id ).ToList();
+            ViewBag.EventsToShow = eventsToShow;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ApprovedExhibition(EventViewModel even)
+        {
+            return View();
+        }
+
+
         [HttpPost]
         public ActionResult SheduleEvent(EventViewModel events)
         {
             var eventType = events.EventType;
             var getEventTypeRow = db.EventTypes.FirstOrDefault(s => s.Name == eventType);
-            var eventState = db.EventStates.Where(s => s.Id == 1).Single();
+            var eventState = db.EventStates.Single(s => s.Id == 1);
             var userId = Int32.Parse(Request.Cookies["UserId"].Value);
             var userAccount = db.UserAccounts.Include(d=>d.Role).FirstOrDefault(s => s.Id == userId);
             var finalEvent = new Event() {Name = events.Name, StartDate = events.StartDate, EnDate = events.EnDate,Description = events.Description,EventType = getEventTypeRow, EventState = eventState,UserAccount = userAccount};
